1c3dcc758305d4073b2d4df31bd9a8342b641dcf
Gianluca Orpello <g.orpello@gmail.com>
2021-01-14 14:43:38 +0100

    First Version

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        registerForKeyboardNotifications()
     }
-
-
+    
+    func registerForKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        
+        
+    }
+    
+    @objc func keyboardWasShown(_ notification: NSNotification) {
+        print(#function)
+    }
+    
 }
 
