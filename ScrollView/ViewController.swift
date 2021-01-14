fadacaad17263903a8f0ff1e26d82d50a161fada
Gianluca Orpello <g.orpello@gmail.com>
2021-01-14 15:58:03 +0100

    Complete v0.1

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FormViewController: UIViewController {
 
+    @IBOutlet weak var formScrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,13 +19,42 @@ class ViewController: UIViewController {
     
     func registerForKeyboardNotifications() {
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWasShown(_:)),
+                                               name: UIResponder.keyboardDidShowNotification,
+                                               object: nil)
         
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillBeHidden(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
         
     }
     
     @objc func keyboardWasShown(_ notification: NSNotification) {
-        print(#function)
+        
+        guard let info = notification.userInfo else {
+            return
+        }
+        
+        guard let keyboardFrame = info[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue else {
+            return
+        }
+        
+        let keybordSize = keyboardFrame.cgRectValue.size
+        
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keybordSize.height, right: 0.0)
+        formScrollView.contentInset = contentInsets
+        formScrollView.scrollIndicatorInsets = contentInsets
+        
+    }
+    
+    @objc func keyboardWillBeHidden(_ notification: NSNotification){
+        
+        let contentInsets = UIEdgeInsets.zero
+        formScrollView.contentInset = contentInsets
+        formScrollView.scrollIndicatorInsets = contentInsets
+        
     }
     
 }
